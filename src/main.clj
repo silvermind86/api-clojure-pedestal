dd526f48c486e672e98f4719fd105fdba71b5441
Jonas Magno Costa <jmagno.costa@gmail.com>
2020-03-19 17:25:53 -0300

    Primeiro Commit.

---
+++ src/main.clj
@@ -0,0 +1,87 @@
+(ns main
+  (:require [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]
+            [io.pedestal.test :as test]))
+
+(defn response [status body & {:as headers}]
+  {:status status :body body :headers headers})
+
+(def ok       (partial response 200))
+(def created  (partial response 201))
+(def accepted (partial response 202))        
+
+(def echo
+  {:name :echo
+   :enter
+   (fn [context]
+     (let [request (:request context)
+           response (ok context)]
+       (assoc context :response response)))})
+
+(defonce database (atom {}))
+
+(def db-interceptor
+  {:name :database-interceptor
+   :enter
+   (fn [context]
+     (update context :request assoc :database @database))                               
+   :leave
+   (fn [context]
+     (if-let [[op & args] (:tx-data context)]                                           
+       (do
+         (apply swap! database op args)                                                 
+         (assoc-in context [:request :database] @database))                             
+       context))})    
+
+(defn make-list [nm]
+  {:name  nm
+   :items {}})
+
+(defn make-list-item [nm]
+  {:name  nm
+   :done? false})
+
+(def list-create
+  {:name :list-create
+   :enter
+   (fn [context]
+     (let [nm       (get-in context [:request :query-params :name] "Unnamed List")      
+           new-list (make-list nm)
+           db-id    (str (gensym "l"))]                                                 
+       (assoc context :tx-data [assoc db-id new-list])))})             
+
+(def routes
+  (route/expand-routes
+   #{["/todo"                    :post   [db-interceptor list-create]] ]
+     ["/todo"                    :get    echo :route-name :list-query-form]
+     ["/todo/:list-id"           :get    echo :route-name :list-view]
+     ["/todo/:list-id"           :post   echo :route-name :list-item-create]
+     ["/todo/:list-id/:item-id"  :get    echo :route-name :list-item-view]
+     ["/todo/:list-id/:item-id"  :put    echo :route-name :list-item-update]
+     ["/todo/:list-id/:item-id"  :delete echo :route-name :list-item-delete]}))
+
+(defn test-request [verb url]
+  (io.pedestal.test/response-for (::http/service-fn @server) verb url))     
+
+(def service-map
+  {::http/routes routes
+   ::http/type   :jetty
+   ::http/port   8890})
+
+(defn start []
+  (http/start (http/create-server service-map)))
+
+(defonce server (atom nil))                                                             
+
+(defn start-dev []
+  (reset! server                                                                        
+          (http/start (http/create-server
+                       (assoc service-map
+                              ::http/join? false)))))                                   
+
+(defn stop-dev []
+  (http/stop @server))
+
+(defn restart []                                                                        
+  (stop-dev)
+  (start-dev))     
